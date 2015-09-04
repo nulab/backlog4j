5b0821b668674bcdb9871b1761d6b8c3edd5cbb9
kim <kim@nulab.co.jp>
2015-09-04 11:29:46 +0900

    Fix PullRequestQueryParams class

--- src/main/java/com/nulabinc/backlog4j/api/option/PullRequestQueryParams.java
+++ src/main/java/com/nulabinc/backlog4j/api/option/PullRequestQueryParams.java
@@ -1,6 +1,5 @@
 package com.nulabinc.backlog4j.api.option;
 
-import com.nulabinc.backlog4j.Activity;
 import com.nulabinc.backlog4j.PullRequest;
 import com.nulabinc.backlog4j.http.NameValuePair;
 
@@ -11,7 +10,7 @@ import java.util.List;
  *
  * @author nulab-inc
  */
-public class PullRequestQueryParams extends QueryParams{
+public class PullRequestQueryParams extends QueryParams {
 
     public PullRequestQueryParams statusType(List<PullRequest.StatusType> statusType) {
         for (PullRequest.StatusType type : statusType) {
@@ -19,20 +18,29 @@ public class PullRequestQueryParams extends QueryParams{
         }
         return this;
     }
+
     @Override
     public PullRequestQueryParams minId(long minId) {
-        return (PullRequestQueryParams)super.minId(minId);
+        return (PullRequestQueryParams) super.minId(minId);
     }
 
+    @Override
     public PullRequestQueryParams maxId(long maxId) {
-        return (PullRequestQueryParams)super.maxId(maxId);
+        return (PullRequestQueryParams) super.maxId(maxId);
     }
 
+    @Override
     public PullRequestQueryParams count(int count) {
-        return (PullRequestQueryParams)super.count(count);
+        return (PullRequestQueryParams) super.count(count);
     }
 
+    @Override
     public PullRequestQueryParams order(Order order) {
-        return (PullRequestQueryParams)super.order(order);
+        return (PullRequestQueryParams) super.order(order);
+    }
+
+    public PullRequestQueryParams offset(long offset) {
+        parameters.add(new NameValuePair("offset", String.valueOf(offset)));
+        return this;
     }
 }
