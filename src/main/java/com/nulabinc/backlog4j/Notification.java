21fad6aeb70812591b6c226c4386719e14a6d9b4
kim <kim@nulab.co.jp>
2015-12-16 10:28:00 +0900

    fix notification reason

--- src/main/java/com/nulabinc/backlog4j/Notification.java
+++ src/main/java/com/nulabinc/backlog4j/Notification.java
@@ -12,7 +12,7 @@ public interface Notification {
     enum Reason {
         Undefined(-1), Assigned(1), Commented(2), IssueCreated(3), IssueUpdated(4),
         FileAttached(5), ProjectUserAdded(6), Other(9),
-        PullRequestAssigned(10), PullRequestCommented(11), PullRequestAdded(12), PullRequestUpdate(13);
+        PullRequestAssigned(10), PullRequestCommented(11), PullRequestAdded(12), PullRequestUpdated(13);
 
         Reason(int intValue) {
             this.intValue = intValue;
