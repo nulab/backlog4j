d0c91a9fba165647a26230dfbe00d452bf129a0e
Yuji MATSUMOTO <matsumoto@nulab.com>
2019-07-16 12:53:18 +0900

    change colors for custom status

--- src/main/java/com/nulabinc/backlog4j/Project.java
+++ src/main/java/com/nulabinc/backlog4j/Project.java
@@ -63,16 +63,20 @@ public interface Project {
     }
 
     enum CustomStatusColor {
-        Color1("#e30000"),
-        Color2("#990000"),
-        Color3("#934981"),
-        Color4("#814fbc"),
-        Color5("#2779ca"),
-        Color6("#007e9a"),
-        Color7("#7ea800"),
-        Color8("#ff9200"),
-        Color9("#ff3265"),
-        Color10("#666665");
+        Color1("#ed8077"),
+        Color2("#4488c5"),
+        Color3("#5eb5a6"),
+        Color4("#b0be3c"),
+        Color5("#ea2c00"),
+        Color6("#e87758"),
+        Color7("#e07b9a"),
+        Color8("#868cb7"),
+        Color9("#3b9dbd"),
+        Color10("#4caf93"),
+        Color11("#b0be3c"),
+        Color12("#eda62a"),
+        Color13("#f42858"),
+        Color14("#393939");
 
         CustomStatusColor(String value) {
             this.value = value;
